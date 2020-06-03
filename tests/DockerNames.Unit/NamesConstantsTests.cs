06a2bcd21d06d438e360789817471d5304b21093
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-06-03 14:00:45 -0300

    feat personalities: Adicionar nomes de cientistas e hackers

---
+++ tests/DockerNames.Unit/NamesConstantsTests.cs
@@ -0,0 +1,22 @@
+﻿
+using FluentAssertions;
+
+using Xunit;
+
+namespace DockerNames.Unit
+{
+    public class NamesConstantsTests
+    {
+        [Fact]
+        public void PersonalitiesContainsOnlyUniqueValues()
+        {
+            // Arrange
+            var subject = Personalities.Values;
+
+            // Act
+
+            // Assert
+            subject.Should().OnlyHaveUniqueItems();
+        }
+    }
+}
