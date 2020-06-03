22a4aa1f2c6e46807f64d359a17af0811d946903
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-06-03 17:37:48 -0300

    refactor: Abstrair lógica para a NameFactoryBase

---
+++ src/DockerNames/NameFactoryBase.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DockerNames
+{
+    /// <summary>
+    /// Base for Name Factories. Inherit this class if you want to customize any aspect of Name Generation.
+    /// </summary>
+    public abstract class NameFactoryBase
+    {
+        protected readonly IEnumerable<string> _left;
+        protected readonly IEnumerable<string> _right;
+        protected readonly Random _rand = new Random();
+
+        protected NameFactoryBase(IEnumerable<string> left, IEnumerable<string> right)
+        {
+            _left = left ?? throw new ArgumentNullException(nameof(left));
+            _right = right ?? throw new ArgumentNullException(nameof(right));
+        }
+
+        /// <summary>
+        /// Builds a new name.
+        /// </summary>
+        /// <param name="separator"></param>
+        /// <returns></returns>
+        public virtual string Build(char separator = '_') => JoinStrings(separator, LeftElement, RightElement);
+
+        /// <summary>
+        /// Joins a number of strings using a given separator.
+        /// </summary>
+        /// <param name="separator"></param>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        protected static string JoinStrings(char separator, params string[] args) => string.Join(separator.ToString(), args);
+
+        /// <summary>
+        /// Picks an index randomly within the collection's bounds.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collection"></param>
+        /// <returns></returns>
+        protected virtual int PickIndex<T>(IEnumerable<T> collection) => _rand.Next(0, collection.Count());
+
+        /// <summary>
+        /// Left Element, picked randomly.
+        /// </summary>
+        protected virtual string LeftElement => _left.ElementAt(PickIndex(_left));
+
+        /// <summary>
+        /// Right element, picked randomly.
+        /// </summary>
+        protected virtual string RightElement => _right.ElementAt(PickIndex(_right));
+    }
+}
--- tests/DockerNames.Unit/DockerNameFactoryTests.cs
+++ tests/DockerNames.Unit/DockerNameFactoryTests.cs
@@ -1,6 +1,4 @@
 ﻿
-using System.Collections.Generic;
-
 using AutoFixture.Xunit2;
 
 using FluentAssertions;
@@ -11,35 +9,32 @@ namespace DockerNames.Unit
 {
     public class DockerNameFactoryTests
     {
-        [Theory, AutoData]
-        public void ConstructableWithEnumerables(IEnumerable<string> left, IEnumerable<string> right)
+        [Fact]
+        public void ConstructableWithoutParams()
         {
             // Arrange
             DockerNameFactory subject;
 
             // Act
-            subject = new DockerNameFactory(left, right);
+            subject = new DockerNameFactory();
 
             // Assert
             subject.Should().NotBeNull();
         }
 
-        [Theory, AutoData]
-        public void BuildsNames(string leftData, string rightData)
+        [Fact]
+        public void BuildsNames()
         {
-            // Arrange
-            IEnumerable<string> left, right;
-            left = new List<string> { leftData };
-            right = new List<string> { rightData };
-            var subject = new DockerNameFactory(left, right);
+            // Arrange            
+            var subject = DockerNameFactory.Instance;
 
             // Act
-            var result = subject.Build();
+            string result = subject.Build();
 
             // Assert
             result.Should()
-                .Contain(leftData).And
-                .Contain(rightData).And
+                .ContainAny(Personalities.Values).And
+                .ContainAny(Adjectives.Values).And
                 .Contain('_'.ToString());
         }
 
@@ -49,11 +44,25 @@ namespace DockerNames.Unit
             // Arrange
 
             // Act
-            var result = subject.Build(separator);
+            string result = subject.Build(separator);
 
             // Assert
             result.Should()
                 .Contain(separator.ToString());
+        }
+
+        [Fact]
+        public void ReturnsAnElement()
+        {
+            // Arrange
+            var subject = DockerNameFactory.Instance;
+
+            // Act
+            string result = subject.Element;
+
+            // Assert
+            result.Should()
+                .NotBeNull();
         }
     }
 }
