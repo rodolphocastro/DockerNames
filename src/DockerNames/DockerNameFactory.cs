34e815a97dac08e6bea420a4909264a613c81c82
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-06-03 16:40:52 -0300

    feat names: Adicionar factory para DockerNames

---
+++ src/DockerNames/DockerNameFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("DockerNames.Unit")]
+namespace DockerNames
+{
+    /// <summary>
+    /// Factory for building Docker names.
+    /// </summary>
+    public class DockerNameFactory
+    {
+        private readonly Random _rand = new Random();
+        private readonly IEnumerable<string> _left;
+        private readonly IEnumerable<string> _right;
+
+        private DockerNameFactory() { }
+
+        protected internal DockerNameFactory(IEnumerable<string> left, IEnumerable<string> right)
+        {
+            _left = left ?? throw new ArgumentNullException(nameof(left));
+            _right = right ?? throw new ArgumentNullException(nameof(right));
+        }
+
+        public virtual string Build(char separator = '_') => string.Join(separator.ToString(), PickLeft(), PickRight());
+
+        protected virtual string PickLeft() => _left.ElementAt(PickIndex(_left));
+
+        protected virtual string PickRight() => _right.ElementAt(PickIndex(_right));
+
+        protected virtual int PickIndex<T>(IEnumerable<T> collection) => _rand.Next(0, collection.Count());
+
+        public static DockerNameFactory Instance { get; } = new DockerNameFactory(Adjectives.Values, Personalities.Values);
+    }
+}
---
+++ tests/DockerNames.Unit/DockerNameFactoryTests.cs
@@ -0,0 +1,59 @@
+﻿
+using System.Collections.Generic;
+
+using AutoFixture.Xunit2;
+
+using FluentAssertions;
+
+using Xunit;
+
+namespace DockerNames.Unit
+{
+    public class DockerNameFactoryTests
+    {
+        [Theory, AutoData]
+        public void ConstructableWithEnumerables(IEnumerable<string> left, IEnumerable<string> right)
+        {
+            // Arrange
+            DockerNameFactory subject;
+
+            // Act
+            subject = new DockerNameFactory(left, right);
+
+            // Assert
+            subject.Should().NotBeNull();
+        }
+
+        [Theory, AutoData]
+        public void BuildsNames(string leftData, string rightData)
+        {
+            // Arrange
+            IEnumerable<string> left, right;
+            left = new List<string> { leftData };
+            right = new List<string> { rightData };
+            var subject = new DockerNameFactory(left, right);
+
+            // Act
+            var result = subject.Build();
+
+            // Assert
+            result.Should()
+                .Contain(leftData).And
+                .Contain(rightData).And
+                .Contain('_'.ToString());
+        }
+
+        [Theory, AutoData]
+        public void BuildNamesWithSeparator(DockerNameFactory subject, char separator)
+        {
+            // Arrange
+
+            // Act
+            var result = subject.Build(separator);
+
+            // Assert
+            result.Should()
+                .Contain(separator.ToString());
+        }
+    }
+}
