3527c4f3e090d71c86bf40c507363feff2f1c2fa
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-06-03 21:50:48 -0300

    feat sample: Adicionar exemplo de utilização

---
+++ samples/DockerNames.CLI/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DockerNames.CLI
+{
+    internal class Program
+    {
+        private static readonly DockerNameFactory _nameFactory = DockerNameFactory.Instance;
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Generating 10 random docker names using the Element Property!");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{i}:{_nameFactory.Element}");
+            }
+
+            Console.WriteLine("Generating 10 random docker names using the Build Method!");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{i}:{_nameFactory.Build()}");
+            }
+
+            Console.WriteLine("Generating 10 random docker names using the Build Method with a custom separator!");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{i}:{_nameFactory.Build(' ')}");
+            }
+        }
+    }
+}
